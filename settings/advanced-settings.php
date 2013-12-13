5d7bb16fa2643986d2da630e8031100e5389e568
Miky Ma <miky@vainmedia.com>
2013-12-12 18:03:53 -0800

    general and advanced settings style

--- settings/advanced-settings.php
+++ settings/advanced-settings.php
@@ -9,9 +9,27 @@
 	<div class="mcg">
 		<div class="headline">Advanced Settings</div>
 
-		<div class="setting">Sleep Mode <a id="sleep-mode-btn" href="#" class="of-btn"><div class="on">On</div><div class="off">Off</div></a></div>
-		<p>Info about Sleep Mode: In blandit nec libero ut lobortis. Aliquam et eros eleifend urna mollis convallis. Duis orci nisl, gravida at lacus vitae, vehicula laoreet leo. Proin nec sagittis urna.</p>
-		<div class="setting">Sidelobe Mode <a id="sidelobe-mode-btn" href="#" class="of-btn"><div class="on">On</div><div class="off">Off</div></a></div>
-		<p>Info about Sidelobe Mode: Nulla feugiat vestibulum egestas. Integer porttitor est turpis, at convallis nisi tristique ac. Vestibulum ac est a quam pellentesque porta. Nunc id pulvinar metus. Etiam commodo faucibus augue, id placerat elit aliquet non. Sed ac diam enim. Phasellus vitae interdum magna.</p>
+		<div class="setting">
+			<a id="sleep-mode-btn" href="#" class="of-btn"><div class="on">On</div><div class="off">Off</div></a>
+			<p>
+				Sleep Mode<br>
+				Info about Sleep Mode: In blandit nec libero ut lobortis.
+				Aliquam et eros eleifend urna mollis convallis. Duis orci nisl,
+				gravida at lacus vitae, vehicula laoreet leo. Proin nec sagittis
+				urna.
+			</p>
+		</div>
+
+		<div class="setting">
+			<a id="sidelobe-mode-btn" href="#" class="of-btn"><div class="on">On</div><div class="off">Off</div></a>
+			<p>
+				Sidelobe Mode<br>
+				Info about Sidelobe Mode: Nulla feugiat vestibulum egestas. Integer
+				porttitor est turpis, at convallis nisi tristique ac. Vestibulum ac
+				est a quam pellentesque porta. Nunc id pulvinar metus. Etiam commodo
+				faucibus augue, id placerat elit aliquet non. Sed ac diam enim.
+				Phasellus vitae interdum magna.
+			</p>			
+		</div>
 	</div>
 </div>
--- settings/general-settings.php
+++ settings/general-settings.php
@@ -8,9 +8,27 @@
 
 	<div class="mcg">
 		<div class="headline">General Settings</div>
-		<div class="setting">Technician Mode<a id="technician-mode-btn" href="#" class="of-btn"><div class="on">On</div><div class="off">Off</div></a></div>
-		<p>Info about Technician Mode: Nulla feugiat vestibulum egestas. Integer porttitor est turpis, at convallis nisi tristique ac. Vestibulum ac est a quam pellentesque porta. Nunc id pulvinar metus. Etiam commodo faucibus augue, id placerat elit aliquet non. Sed ac diam enim. Phasellus vitae interdum magna.</p>
-		<div class="setting">Demo Mode<a id="demo-mode-btn" href="#" class="of-btn"><div class="on">On</div><div class="off">Off</div></a></div>
-		<p>Info about Demo Mode: In blandit nec libero ut lobortis. Aliquam et eros eleifend urna mollis convallis. Duis orci nisl, gravida at lacus vitae, vehicula laoreet leo. Proin nec sagittis urna.</p>
+
+		<div class="setting">
+			<a id="technician-mode-btn" href="#" class="of-btn"><div class="on">On</div><div class="off">Off</div></a>
+			<p>
+				Technician Mode<br>
+				Info about Technician Mode: Nulla feugiat vestibulum egestas.
+				Integer porttitor est turpis, at convallis nisi tristique ac.
+				Vestibulum ac est a quam pellentesque porta. Nunc id pulvinar
+				metus. Etiam commodo faucibus augue, id placerat elit aliquet
+				non. Sed ac diam enim. Phasellus vitae interdum magna.
+			</p>
+		</div>
+
+		<div class="setting">
+			<a id="demo-mode-btn" href="#" class="of-btn"><div class="on">On</div><div class="off">Off</div></a>
+			<p>
+				Demo Mode<br>
+				Info about Demo Mode: In blandit nec libero ut lobortis. Aliquam
+				et eros eleifend urna mollis convallis. Duis orci nisl, gravida
+				at lacus vitae, vehicula laoreet leo. Proin nec sagittis urna.
+			</p>
+		</div>
 	</div>
 </div>
