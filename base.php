6b377dc65a4440a33de46f32444b88912b420aac
Miky Ma <miky@vainmedia.com>
2013-12-17 11:12:30 -0800

    added no-cache meta tags to base.php

--- base.php
+++ base.php
@@ -4,6 +4,13 @@
 		<title>KVH TVRO</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+
+		<meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
+
 		<link type="text/css" rel="stylesheet" href="/css/base.css">
 		<script type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>
 		<script type="text/javascript" src="/js/base.js"></script>
