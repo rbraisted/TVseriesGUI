c94445a2c5b6bae489c20533d298a522fce18110
miky <miky@localhost>
2014-02-03 19:02:06 -0800

    wizard step 1 initial css

---
+++ wizard.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>KVH TVRO</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+
+		<meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
+		
+		<link type="text/css" rel="stylesheet" href="/css/base.css">
+		<link type="text/css" rel="stylesheet" href="/css/wizard.css">
+
+		<script type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>
+		<script type="text/javascript" src="/js/base.js"></script>
+		
+	</head>
+	<body>
+
+<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+		<div id="header" class="view header-view">
+			<a href="/" class="btn tracvision-logo">
+				<img src="/images/img.gif">
+			</a>
+		</div>
+
+<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+
+		<div id="wizard-title" class="wizard-title-view">
+			<span class="mobile">Welcome To The Wizard</span>
+			<span class="desktop">Welcome to The TracVision Setup Wizard</span>
+		</div>
+		
+		<div id="wizard-content" class="wizard-content-view">		
+			<div id="first-steps" class="first-steps-view">
+				<span class="first-steps-header">Before you start the Wizard,<br/>please do the following:</span>
+				<div class="step">
+					<div class="step-icon">1</div>
+					<div class="step-copy">Identify the desired service provider & associated satellite(s)</div>
+				</div>
+				<div class="step">
+					<div class="step-icon">2</div>
+					<div class="step-copy">Download the latest antenna software</div>
+				</div>
+				<div class="step">
+					<div class="step-icon">3</div>
+					<div class="step-copy">Connect all system components for your particular configuration</div>
+				</div>
+			</div>
+		</div>
+
+<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+
+	</body>
+</html>
