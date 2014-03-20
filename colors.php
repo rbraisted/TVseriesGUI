e8ea6ee0dc4b09d5a26db9eb63f9e97f722f132f
Miky Ma <miky@vainmedia.com>
2014-03-20 15:02:05 -0700

    added colors.php for reference

---
+++ colors.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<style>
+		div {
+			width: 100%;
+			line-height: 50px;
+			margin-top: 25px;
+			margin-bottom: 25px;
+			text-indent: 25px;
+			font-family: sans-serif;
+			font-weight: bold;
+			color: #fff;
+		}
+	</style>
+	<body>
+		<div style="background: #acd038">green</div>
+		<div style="background: #c51313">red</div>
+		<div style="background: #4f7abd">bright-blue</div>
+		<div style="background: #243774">light-blue</div>
+		<div style="background: #1c335d">dark-blue</div>
+		<div style="background: #101f30">grey-blue</div>
+		<div style="background: #293147">dark-grey</div>
+		<div style="background: #4c5269">light-grey</div>
+		<div style="background: #bac7da">bright-grey</div>
+	</body>
+</html>
