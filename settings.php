fe73f401c3b14c974bced2b993c0b50b8634fd3b
jschmidt1 <jschmidt1@localhost>
2014-04-29 14:00:06 -0400

    Internal JIRA SMTV-402, change sidelobe text.

--- settings.php
+++ settings.php
@@ -82,11 +82,12 @@
     <div class="off">Off</div>
   </div>
 
-  <div class="heading">Sidelobe Mode</div>
+  <div class="heading">Sidelobe Mode (TV6 only)</div>
   <p>
-    Sidelobe Mode ensures the antenna is always tracking the main beam of the
-    satellite, and not on a weaker sidelobe. For optimum performance, keep
-    Sidelobe Mode enabled unless directed otherwise by KVH Technical Support.
+    Sidelobe Mode ensures the antenna is tracking the main beam of the satellite,
+    and not a weaker sidelobe. Since sidelobes are not strong enough to become a
+    problem in most regions, keep Sidelobe Mode set to off unless directed
+    otherwise by KVH Technical Support.
   </p>
   <div class="toggle-btn #sidelobe-mode-btn">
     <div class="on">On</div>
