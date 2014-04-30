1022afea917d39e4c9726c0eb34da7488470fdbc
miky <miky@localhost>
2014-04-30 12:44:29 -0700

    wiz autoswitch page update

--- js/wizard/autoswitch.js
+++ js/wizard/autoswitch.js
@@ -1,6 +1,93 @@
-"use strict";
+$(function() {
 
-/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+  var prevBtn = $('.\\#prev-btn').click(function() {
+    window.history.go(-1);
+  });
+
+  var nextBtn = $('.\\#next-btn').click(function() {
+    window.location = '/wizard/activation.php';
+  });
+
+  var receiverTableView = TVRO.ReceiverTableView(
+    $('.\\#receiver-table-view')
+      .find('.\\#edit-btn')
+        .click(function(event) {
+          event.stopPropagation();
+          var index = $('.\\#receiver-table-view .\\#edit-btn').index(this);
+          var receiver = encode(receiverTableView.getValues()[index].id);
+          window.location.hash = '/' + receiver + '/edit';
+        })
+        .end()
+  ).onClick(function(receiver) {
+    window.location.hash = '/' + encode(receiver.id);
+  });
+
+  var receiverInfoView = TVRO.ReceiverInfoView(
+    $('.\\#receiver-info-view')
+      .find('.\\#back-btn')
+        .click(function() {
+          window.location.hash = '';
+        })
+        .end()
+      .find('.\\#edit-btn')
+        .click(function() {
+          var receiver = encode(receiverInfoView.getReceiver().id);
+          window.location.hash = '/' + receiver + '/edit';
+        })
+        .end()
+  );
+
+  var receiverEditView = TVRO.ReceiverEditView(
+    $('.\\#receiver-edit-view')
+      .find('.\\#back-btn')
+        .click(function() {
+          var receiver = encode(receiverEditView.getReceiver() ? receiverEditView.getReceiver().id : '');
+          window.location.hash = (receiver ? '/' + receiver : '');
+        })
+        .end()
+  );
+
+  var createReceiverBtn = $('.\\#new-btn').click(function(argument) {
+    window.location.hash = '/new/edit';
+  });
+
+
+
+  //  init
+
+  receiverTableView.reload();
+
+  TVRO.getReceiverType().then(function(receiverType) {
+    $('.\\#receiver-type').text(receiverType);
+  });
+
+  TVRO.onHashChange(function(hash) {
+    var split = _.rest(hash.split('/'));
+
+    if (!hash) {
+      document.body.className = '';
+
+    } else if (hash.match(/\/new/)) {
+      receiverEditView.createNew();
+      document.body.className = '/receiver/edit';
+
+    } else if (hash.match(/\/edit/)) {
+      receiverEditView.setReceiver({ id: split[0] });
+      document.body.className = '/receiver/edit';
+
+    } else if (hash.match(/\/.*/)) {
+      receiverInfoView.setReceiver({ id: split[0] });
+      document.body.className = '/receiver';
+    }
+  });
+
+  TVRO.reload();
+});
+
+
+/*
+
+"use strict";
 
 TVRO.AutoswitchPage = function() {
 	var 
@@ -229,6 +316,5 @@ TVRO.AutoswitchPage = function() {
 	}
 };
 
-/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
-
 TVRO.page = TVRO.AutoswitchPage();
+*/
--- wizard/autoswitch.php
+++ wizard/autoswitch.php
@@ -1,352 +1,145 @@
-<? include $_SERVER['DOCUMENT_ROOT'] . '/wizard.php'; ?>
+<?
+$wiz = 1;
+include $_SERVER['DOCUMENT_ROOT'] . '/base_.php';
+?>
+
+<script type="text/javascript" src="/js/ReceiverTableView.js"></script>
+<script type="text/javascript" src="/js/ReceiverInfoView.js"></script>
+<script type="text/javascript" src="/js/ReceiverEditView.js"></script>
+
 <script type="text/javascript" src="/js/wizard/autoswitch.js"></script>
-<script type="text/javascript" src="/js/wizard/autoswitch.js"></script>
 
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
 
-<div id="autoswitches-view" class="view main-view wiz-view autoswitches-view">
-    <div class="view-content main-content wiz-content">
-        <div class="wiz-title-view tac bb dfs26 mfs21">Autoswitching Setup</div>
-        <div class="dmaxw700 mc">
-            <div class="dfs13 dlh1.4 mfs16 mlh1.4 mt3 mb2 wiz-instructions">
-                IP AutoSwitch(es) have been detected on your network. Choose the IP
-                AutoSwitch you would like to be the master (controlling satellite
-                selection). If you like, you can also name the IP AutoSwitch(es)
-                to make them easier to remember.
-                <br>
-                <br>
-                To determine the location of an IP AutoSwitch, click its
-                <a class="wiz-link" href="/media/IPAS_Master_Select_Button.png">
-                 master button
-                </a>
-                 and that IP AutoSwitch will be highlighted in the table below. Once
-                you’ve named all the IP AutoSwitch(es), dont forget to choose the
-                master.
-            </div>
-            <div id="table-rows">
-                <div id="template table-row" class="device-table-row">
-                    <div class="device-table-row-status">
-                        <div class="status-icon"></div>
-                        <div class="status-label">Status</div>
-                    </div>
-                    <div class="device-table-row-name">
-                        <div class="name-label">IP Autoswitch</div>
-                        <div id="name" class="name-text"></div>
-                    </div>
-                    <div class="device-table-row-id">
-                        <div class="id-label">Serial #</div>
-                        <div id="serial-number" class="id-text"></div>
-                    </div>
-                    <div class="device-table-row-master">
-                        <div class="master-label">Master</div>
-                        <div class="master-icon"></div>
-                    </div>
-                    <div id="view-btn" class="btn basic-btn view-btn"></div>
-                    <div id="edit-btn" class="btn basic-btn edit-btn">
-                        <label>Edit</label>
-                    </div>
-                    <div id="select-btn" class="btn basic-btn master-btn">
-                        <label>Select</label>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div class="view receiver-table-view #receiver-table-view">
+  <div class="view-head">AutoSwitching Setup</div>
+
+  <div class="copy #ip-autoswitch">
+    The following IP AutoSwitch(es) have been found or were previously
+    configured on your network. The receiver connected to the master IP
+    AutoSwitch controls satellite selection in automatic satellite
+    switching mode. You may choose the master by selecting it below or
+    by pressing the
+    <a class="link" href="/media/IPAS_Master_Select_Button.png">
+     Master Select button
+    </a>
+    on the IP AutoSwitch itself,
+    provided it is currently active on the network (status is green).
+    Assign a unique name to each IP AutoSwitch so you can easily identify
+    it later.
+    <div class="note">
+      Hint: To determine the location of an IP AutoSwitch, press its
+      Master Select button. That IP AutoSwitch will then become the
+      master in the list below.
     </div>
-    <div class="bottom-bar">
-        <a id="prev-btn" class="btn prev-btn prev-icon fl">Previous</a>
-        <a id="next-btn" class="btn next-btn next-icon fr">Next</a>
+  </div>
+
+  <div class="copy #directv">
+    The following receivers have been set up for automatic satellite
+    switching. The master receiver controls satellite selection. You may
+    choose the master by selecting it below. To add a receiver to the
+    list, enter its IP address and assign it a unique name.
+    <a class="link" href="/media/DTV_FindIPaddress.mp4">
+      Find your receiver’s IP address.
+    </a>
+    <div class="note">
+      Note: Receiver must be activated to view its IP address. The
+      receiver also must be set up to allow external access before it
+      will communicate with the TV-Hub.
+      <a class="link" href="/media/DTV_ExternalControl.mp4">
+        Learn how to enable external access on your DIRECTV receiver.
+      </a>
     </div>
+  </div>
+
+  <div class="receiver-table #table-view">
+    <div class="table-row #table-row">
+      <div class="table-col install-btn #select-btn"></div><!--
+   --><div class="table-col status-col">
+        <div class="status-icon label">Status</div>
+      </div><!--
+   --><div class="table-col name-col">
+        <div class="label #receiver-name-label"></div>
+        <div class="value #receiver-name"></div>
+      </div><!--
+   --><div class="table-col id-col">
+        <div class="label #receiver-id-label"></div>
+        <div class="value #receiver-id"></div>
+      </div><!--
+   --><div class="table-col edit-col">
+        <div class="block-btn edit-btn #edit-btn">Edit</div>
+      </div><!--
+   --><div class="table-col select-col">
+        <div class="block-btn select-btn #select-btn">Select</div>
+        <div class="master-icon label">Master</div>
+      </div>
+    </div>
+  </div>
+
+  <div class="block-btn new-btn #new-btn">
+    Add
+    <span class="#receiver-type"></span>
+  </div><!--
+--><div class="come-back-copy copy">
+    You can come back to this later if you are not ready to finish the AutoSwitching setup.
+  </div>
+
+  <div class="bottom-bar">
+    <div class="prev-btn #prev-btn">Previous</div>
+    <div class="next-btn #next-btn">Next</div>
+  </div>
 </div>
 
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
 
-<div id="autoswitch-view" class="view main-view wiz-view autoswitch-view">
-    <div class="view-content main-content wiz-content">
-        <a id="back-btn" class="btn back-btn">
-            <img src="/images/img.gif">
-            <label id="not-directv">AutoSwitch Details</label>
-            <label id="directv">Receiver Details</label>
-        </a>
-        <h1>
-            <img src="/images/img.gif">
-            <span id="not-directv">AutoSwitch Details</span>
-            <span id="directv">Receiver Details</span>
-        </h1>
-
-        <div class="label receiver-label">
-            <span id="not-directv">IP Autoswitch</span>
-            <span id="directv">Receiver</span>
-        </div>
-        <div id="name" class="value receiver-value"></div>
-
-        <div id="not-directv" class="label serial-number-label">Serial #</div>
-        <div id="not-directv serial-number" class="value serial-number-value"></div>
-
-        <div id="directv" class="label serial-number-label">IP Address</div>
-        <div id="directv ip" class="value serial-number-value"></div>
-
-        <div class="label status-label">Status</div>
-        <img src="/images/img.gif" class="status-img">
-
-        <div class="label master-label">Master</div>
-        <img src="/images/img.gif" class="master-img">
-
-        <a id="edit-btn" class="btn basic-btn edit-btn">
-            <label>Edit</label>
-        </a>
-
-        <a id="select-btn" class="btn basic-btn select-btn">
-            <label>Select</label>
-        </a>
+<div class="popup #receiver-info-view">
+  <div class="popup-guts receiver-info">
+    <div class="view-head">
+      <span class="autoswitch-icon"></span>
+      <span class="#ip-autoswitch">IP AutoSwitch</span>
+      <span class="#directv">Receiver</span>
+      <div class="back-btn #back-btn"></div>
     </div>
+
+    <div class="label #receiver-name-label"></div>
+    <div class="value #receiver-name"></div>
+
+    <div class="label #receiver-id-label"></div>
+    <div class="value #receiver-id"></div>
+
+    <div class="status-icon label">Status</div>
+
+    <div class="btn-tray">
+      <div class="block-btn first edit-btn #edit-btn">Edit</div><!--
+   --><div class="block-btn select-btn #select-btn">Select</div><!--
+   --><div class="master-icon label">Master</div>
+    </div>
+  </div>
 </div>
 
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
 
-<div id="autoswitch-edit-view" class="view popup-view autoswitch-edit-view">
-    <div class="view-content popup-content">
-        <a id="cancel-btn" class="btn back-btn">
-            <img src="/images/img.gif">
-            <label>
-                <span id="new">Add</span>
-                <span id="old">Edit</span>
-                <span id="not-directv">IP Autoswitch</span>
-                <span id="directv">Receiver</span>
-            </label>
-        </a>
-        <h1>
-            <span id="new">Add</span>
-            <span id="old">Edit</span>
-            <span id="not-directv">IP Autoswitch</span>
-            <span id="directv">Receiver</span>
-        </h1>
-
-        <p class="copy">KVH recommends that you run the Setup Wizard whenever you change your
-            system's configuration by adding or removing devices.</p>
-
-        <a id="wizard-btn" class="btn basic-btn wizard-btn">
-            <label>Launch Setup Wizard</label>
-        </a>
-
-        <div class="guts">
-            <div class="label receiver-label">
-                <span id="not-directv">IP Autoswitch</span>
-                <span id="directv">Receiver</span>
-            </div>
-            <input id="name" type="text" class="value receiver-value">
-
-            <div id="not-directv" class="label serial-number-label">Serial #</div>
-            <input id="not-directv serial-number" type="text" class="value serial-number-value">
-
-            <div id="directv" class="label serial-number-label">IP Address</div>
-            <input id="directv ip" type="text" class="value serial-number-value">
-        </div>
-
-        <a id="old delete-btn" class="btn basic-btn delete-btn">
-            <label>Remove</label>
-        </a>
-
-        <a id="cancel-btn" class="btn basic-btn cancel-btn">
-            <label>Cancel</label>
-        </a>
-
-        <a id="save-btn" class="btn basic-btn save-btn">
-            <label>Save</label>
-        </a>
+<div class="popup #receiver-edit-view">
+  <div class="popup-guts receiver-edit">
+    <div class="view-head">
+      <span class="autoswitch-icon"></span>
+      <span class="#ip-autoswitch">IP AutoSwitch</span>
+      <span class="#directv">Receiver</span>
+      <div class="back-btn #back-btn"></div>
     </div>
+
+    <div class="label name-label #receiver-name-label"></div>
+    <input class="input name-input #receiver-name" />
+
+    <div class="label id-label #receiver-id-label"></div>
+    <input class="input id-input #receiver-id" />
+
+    <div class="status-icon label">Status</div>
+    <div class="block-btn remove-btn #delete-btn">Remove</div>
+
+    <div class="btn-tray">
+      <div class="block-btn cancel-btn first #back-btn">Cancel</div><!--
+   --><div class="block-btn save-btn #save-btn">Save</div>
+    </div>
+  </div>
 </div>
-
-<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
-
-<style type="text/css">
-    @media screen and (min-width: 880px) {
-        body.at-splash .autoswitches-view,
-        body.at-autoswitches-view .autoswitches-view,
-        body.at-autoswitch-view .autoswitches-view,
-        body.at-autoswitch-edit-view .autoswitches-view,
-        body.at-autoswitch-edit-view .autoswitch-edit-view {
-            display: block;
-        }
-    }
-
-    @media screen and (max-width: 880px) {
-        body.at-splash .autoswitches-view,
-        body.at-autoswitches-view .autoswitches-view,
-        body.at-autoswitch-view .autoswitch-view,
-        body.at-autoswitch-edit-view .autoswitch-edit-view {
-            display: block;
-        }
-    }
-
-    .device-table-row {
-        background: #000;
-        border: 1px solid #c3140f;
-        height: 85px;
-        margin-top: 10px;
-        position: relative;
-    }
-
-    .device-table-row.is-active {
-        border-color: #acd038;
-    }
-
-    .device-table-row.is-master {
-        background-color: #4e79bc;
-        border-color: #4e79bc;
-    }
-
-    .device-table-row-status,
-    .device-table-row-name,
-    .device-table-row-id,
-    .device-table-row-master,
-    .device-table-row .basic-btn {
-        position: absolute;
-        top: 50%;
-    }
-
-    .device-table-row-status {
-        left: 20px;
-        margin-top: -10px;
-    }
-
-    .device-table-row-name {
-        left: 120px;
-        margin-top: -20px;
-    }
-
-    .device-table-row-id {
-        left: 280px;
-        margin-top: -20px;
-    }
-
-    .device-table-row-master {
-        right: 20px;
-        margin-top: -10px;
-    }
-
-    .device-table-row .status-icon,
-    .device-table-row .status-label,
-    .device-table-row .master-icon,
-    .device-table-row .master-label {
-        display: inline-block;
-        vertical-align: middle;
-        font-size: 13px;
-    }
-
-    .device-table-row .status-label,
-    .device-table-row .master-label {
-        margin: 0 5px 0;
-    }
-
-    .device-table-row .name-label,
-    .device-table-row .id-label {
-        font-size: 13px;
-        margin-bottom: 10px;
-    }
-
-    .device-table-row .status-icon,
-    .device-table-row .master-icon {
-        background: transparent no-repeat center/21px;
-        min-width: 21px;
-        min-height: 21px;
-    }
-
-    .device-table-row .status-icon {
-        background-image: url(/images/red-dot-icon.svg);
-    }
-
-    .device-table-row.is-active .status-icon {
-        background-image: url(/images/green-dot-icon.svg);
-    }
-
-    .device-table-row .master-icon {
-        background-image: url(/images/radio.png);
-    }
-
-    .device-table-row.is-master .master-icon {
-        background-image: url(/images/radio-selected.png);
-    }
-
-    .device-table-row .basic-btn {
-        min-width: 95px;
-        margin-top: -20px;
-    }
-
-    .device-table-row .edit-btn {
-        right: 135px;
-    }
-
-    .device-table-row .master-btn {
-        right: 20px;
-    }
-
-    .device-table-row.is-master .master-btn {
-        display: none;
-    }
-
-    .device-table-row .view-btn {
-        display: none;
-    }
-
-    @media screen and (max-width: 880px) {
-        .device-table-row,
-        .device-table-row.is-active {
-            background: #293047 url(/images/menu-btn.png) no-repeat center right 10px/7px 8px;
-            border-color: #4e79bc;
-            border-width: 0 0 1px;
-            height: 50px;
-            margin: 0;
-        }
-
-        .device-table-row.is-master {
-            background-color: #4e79bc;
-            background-image: url(/images/menu-btn-selected.png);
-            border-color: #4e79bc;
-        }
-
-        .device-table-row-status,
-        .device-table-row-master,
-        .device-table-row .name-label,
-        .device-table-row-id,
-        .device-table-row .edit-btn,
-        .device-table-row .master-btn label {
-            display: none;
-        }
-
-        .device-table-row-name {
-            left: 60px;
-            margin-top: -0.5em;
-        }
-
-        .device-table-row .master-btn {
-            background: transparent url(/images/radio.png) no-repeat center/21px;
-            height: 21px;
-            left: 15px;
-            margin-top: -10px;
-            min-width: 0;
-            position: absolute;
-            top: 50%;
-            width: 21px;
-        }
-
-        .device-table-row.is-master .master-btn {
-            background-image: url(/images/radio-selected.png);
-            display: block;
-        }
-
-        .device-table-row .master-btn label {
-            display: none;
-        }
-
-        .device-table-row .view-btn {
-            background-color: transparent;
-            display: block;
-            height: 100%;
-            left: 0;
-            position: absolute;
-            top: 0;
-            width: 100%;
-            margin: 0;
-        }
-    }
-</style>
