902dfd20e26e721bfa023bda005f37b04f2b266f
miky <miky@localhost>
2014-05-01 14:46:26 -0700

    fix for set_wlan AP and IF modes

--- js/WirelessSettingsView.js
+++ js/WirelessSettingsView.js
@@ -14,42 +14,61 @@
     var saveBtn = $('.\\#save-btn', jQ).click(function() {
       //  we make this call just to get <security><algorithm>
       //  the call to set <security><wpa2> does not work without it
-      var mode = $('.\\#wlan-mode', jQ).text();
-      var essid = $('.\\#wlan-essid', jQ).val();
-      var ip = $('.\\#wlan-ip', jQ).val();
-      var netmask = $('.\\#wlan-netmask', jQ).val();
-      var gateway = $('.\\#wlan-gateway', jQ).val();
+      TVRO.getWlan().then(function(xml) {
+        var mode = $('.\\#wlan-mode', jQ).text();
+        var essid = $('.\\#wlan-essid', jQ).val();
+        var ip = $('.\\#wlan-ip', jQ).val();
+        var netmask = $('.\\#wlan-netmask', jQ).val();
+        var gateway = $('.\\#wlan-gateway', jQ).val();
 
-      //  if mode is AP, ap_mode mode .text() else if_mode mode .text()
-      var networkMode = $('.\\#wlan-network-mode', jQ).text();
-      var securityMode = $('.\\#wlan-security-mode', jQ).text();
-      var securityKey = $('.\\#wlan-security-key', jQ).text();
+        //  if mode is AP, ap_mode mode .text() else if_mode mode .text()
+        var networkMode = $('.\\#wlan-network-mode', jQ).text();
+        var securityMode = $('.\\#wlan-security-mode', jQ).text();
+        var securityKey = $('.\\#wlan-security-key', jQ).text();
 
-      if (mode === 'AP (Access Point)') xml = $('ap_mode', xml);
-      else xml = $('if_mode', xml);
+        //  reassign xml here otherwise
+        //  you'll pull the <security><algorithm> from both modes (IF and AP)
+        if (mode === 'AP (Access Point)') xml = $('ap_mode', xml);
+        else xml = $('if_mode', xml);
 
-      var params = {
-        mode: mode,
-        if_mode: {
-          mode: networkMode,
-          essid: essid,
-          ip: ip,
-          netmask: netmask,
-          gateway: gateway,
-          security: {
-            mode: securityMode,
-            wpa2: securityKey
+        var securityAlgorithm = $('security algorithm', xml).text();
+
+        //  after a bunch of tests
+        //  here is what i've figured out:
+        //  1. you should not send "broadcast"
+        //     it results in an error 4 (required tag missing)
+        //     at least when setting AP mode
+        //  2. you should send algorithm
+        //     at least when setting IF mode
+        //  3. <wpa2> works when setting AP,
+        //     <key> works when setting IF
+        //     sending both in both cases doesn't hurt
+        //     so just send both
+        var params = {
+          mode: mode,
+          if_mode: {
+            mode: networkMode,
+            essid: essid,
+            ip: ip,
+            netmask: netmask,
+            gateway: gateway,
+            security: {
+              mode: securityMode,
+              algorithm: securityAlgorithm,
+              wpa2: securityKey,
+              key: securityKey
+            }
           }
+        };
+
+        if (mode === 'AP (Access Point)') {
+          params.ap_mode = params.if_mode;
+          delete params.if_mode;
         }
-      };
 
-      if (mode === 'AP (Access Point)') {
-        params.ap_mode = params.if_mode;
-        delete params.if_mode;
-      }
-
-      var confirmed = confirm('Are you sure you want to save these changes?');
-      if (confirmed) TVRO.setWlan(params).then(TVRO.reload);
+        var confirmed = confirm('Are you sure you want to save these changes?');
+        if (confirmed) TVRO.setWlan(params).then(TVRO.reload);
+      });
     });
 
 
@@ -177,3 +196,77 @@
 	TVRO.WirelessSettingsView = WirelessSettingsView;
 
 }(window.TVRO);
+
+/*
+
+<ipacu_response>
+  <message name="get_wlan" error="0"></message>
+  <mode>AP (Access Point)</mode>
+  <if_mode>
+    <mode>DYNAMIC</mode>
+    <essid>linksys</essid>
+    <security>
+      <mode>OFF</mode>
+      <algorithm>TKIP</algorithm>
+      <key>tracvision</key>
+    </security>
+    <ip>169.254.1.1</ip>
+    <netmask>255.255.0.0</netmask>
+    <gateway>169.254.1.1</gateway>
+    <broadcast>169.254.1.255</broadcast>
+  </if_mode>
+</ipacu_response>
+
+{
+  mode: "IF (Infrastructure)",
+  if_mode: {
+    mode: "DYNAMIC",
+    essid: "linksys",
+    security: {
+      mode: "OFF",
+      algorithm: "TKIP",
+      key: "tracvision"
+    },
+    ip: "169.254.1.1",
+    netmask: "255.255.0.0",
+    gateway: "169.254.1.1",
+    broadcast: "169.254.1.255"
+  }
+}
+
+<ipacu_request>
+  <message name="set_wlan"></message>
+  <mode>AP (Access Point)</mode>
+  <ap_mode>
+    <mode>ENDPOINT</mode>
+    <essid>TVHub-140311659</essid>
+    <ip>172.16.0.1</ip>
+    <netmask>255.255.255.0</netmask>
+    <gateway>172.16.0.1</gateway>
+    <broadcast>172.16.0.1</broadcast>
+    <security>
+      <mode>OFF</mode>
+      <wpa2>tracvision</wpa2>
+      <key>tracvision</key>
+    </security>
+</ap_mode>
+</ipacu_request>
+
+{
+  mode: "AP (Access Point)",
+  ap_mode: {
+    mode: "ENDPOINT",
+    essid: "TVHub-140311659",
+    ip: "172.16.0.1",
+    netmask: "255.255.255.0",
+    gateway: "172.16.0.1",
+    security: {
+      mode: "WPA_PSK",
+      algorithm: "TKIP",
+      wpa2: "securitykey",
+      key: "securitykey"
+    }
+  }
+}
+
+*/
