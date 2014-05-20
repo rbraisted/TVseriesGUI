461a139e42379317f12f37f6833ef51dc2541916
jschmidt1 <jschmidt1@localhost>
2014-05-20 15:56:57 -0400

    Internal JIRA 478, Changed white text on white background to black text

--- print2screen.php
+++ print2screen.php
@@ -7,7 +7,7 @@
     <script language="javascript" type="text/javascript">
 
         // Scrolls the page as the data from the antenna is received
-        function pageScroll() 
+        function pageScroll()
         {
             window.scrollBy(0,10); // horizontal and vertical scroll increments
             //scrolldelay = setTimeout('pageScroll()',100); // scrolls every 100 milliseconds
@@ -15,17 +15,17 @@
             objDiv.scrollTop = objDiv.scrollHeight;
 
         }
-		
+
 		// Tells the Telnet socket below to close
 		function pageClosing()
 		{
 			document.myTelnet.exitingPage.value = "close";
-			document.myTelnet.submit(); 
-			
+			document.myTelnet.submit();
+
 		}
-		
-		
-		
+
+
+
 		//pageScroll();
 		setInterval(pageScroll, 100);
 
@@ -35,50 +35,50 @@
 <body onunload="pageClosing()" onload="pageScroll()">
   <style type="text/css">
     body {
-      color: #fff;
+      color: #000000;
       font-family: Helvetica, Arial, sans-serif;
       font-size: 16px;
       font-weight: lighter;
       line-height: 1.4;
     }
   </style>
-	<form name="myTelnet" method="POST" action="#"> 
+	<form name="myTelnet" method="POST" action="#">
         <input type="hidden" name="exitingPage" />
         <div id="tnet" style='height:96%; width:98%; position:absolute' >
             <?php
-    
+
                 // Opens a telnet session to receive data from the antenna
                 if ( !isset($_POST["exitingPage"]) ) {
                     $fp = fsockopen("127.0.0.1", 50001, $errno, $errstr, 10);
                 }
-    
+
                 // Reads the data coming from the antenna to send to the screen
 				if ( false !== $fp ) {
-					
+
 					while ( !isset($_POST["exitingPage"]) && false !== $fp ) {
 						echo fgets($fp, 128) . "<br />";
 						flush();
 						ob_flush();
 						//sleep(1);
-						
+
 						if( isset($_POST["exitingPage"]) ) {
-							
+
 							if ( "close" == $_POST["exitingPage"] ) {
 								fclose($fp);
-								
+
 							}
-							
+
 						}
-		
+
 					}
-					
+
 					echo 'Connection lost. Try refreshing the browser (F5)';
-					
+
 				} else {
 					echo 'Cannot connect try refreshing the browser (F5)';
-					
+
 				}
-    
+
             ?>
         </div>
     </form>
