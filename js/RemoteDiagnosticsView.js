b5d1405e47f50ec29a4ea6c42abb8d764f73a99e
miky <miky@localhost>
2014-05-23 18:13:12 -0700

    STWA-214, style is not that great and will probably be redone

--- js/RemoteDiagnosticsView.js
+++ js/RemoteDiagnosticsView.js
@@ -2,10 +2,36 @@
 	"use strict";
 
 	var RemoteDiagnosticsView = function(jQ) {
+    var interval; //  store this interval so we can
+
+    var stopInterval = function() {
+      if (interval) clearInterval(interval);
+      interval = undefined;
+      $('.\\#state', jQ).text(' ');
+    };
+
+    var startInterval = function() {
+      stopInterval();
+      interval = setInterval(function() {
+        TVRO.getCallhome(1, 1).then(function(xml) {
+          var state = $('status', xml).text();
+          $('.\\#state', jQ).text(state);          
+        });
+
+      }, 1000);
+    };
+
     var connectBtn = $('.\\#connect-btn', jQ).click(function() {
       TVRO.setCallhome({
         action: 'CONNECT'
-      }).then(undefined, function() {
+      }).then(function(xml) {
+        xml = $(xml).get(0); //  not sure why, but this isnt returning what i expected from xml
+        var state = $('status', xml).text();
+        $('.\\#state', jQ).text(state);
+
+        startInterval();
+
+      }, function() {
         alert('No internet connection detected.');
       });
     });
@@ -15,6 +41,10 @@
         action: 'DISCONNECT'
       });
     });
+
+    return {
+      stopUpdating: stopInterval
+    };
 	};
 
 	TVRO.RemoteDiagnosticsView = RemoteDiagnosticsView;
