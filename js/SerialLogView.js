28aa45af35134c1e7a98e0ca5b5059fa7a785b68
miky <miky@localhost>
2014-06-03 12:05:29 -0700

    spinner on serial log view

--- js/SerialLogView.js
+++ js/SerialLogView.js
@@ -10,10 +10,10 @@
 
     var emailBtn = $('.\\#email-btn', jQ).click(function() {
       Promise.all(
-          TVRO.getAntennaVersions(),
-          TVRO.getAntennaStatus(),
-          TVRO.getSerialLog(1, 1),
-          TVRO.startSerialLog({ restart: 'N' })
+        TVRO.getAntennaVersions(),
+        TVRO.getAntennaStatus(),
+        TVRO.getSerialLog(1, 1),
+        TVRO.startSerialLog({ restart: 'N' })
       ).then(function(xmls) {
         var email = 'support@kvh.com';
         var antModel = $('au model', xmls[0]).text();
@@ -22,9 +22,9 @@
         var dateTime = $('gps dt', xmls[1]).text();
 
         var subject = encode(
-            'TV-Hub: ' + antModel + ' ' + hubSn +
-            ' Antenna Unit S/N: ' + antSn +
-            ' Date/Time: ' + dateTime
+          'TV-Hub: ' + antModel + ' ' + hubSn +
+          ' Antenna Unit S/N: ' + antSn +
+          ' Date/Time: ' + dateTime
         );
 
         var body = encode($('content', xmls[2]).text());
@@ -32,18 +32,21 @@
         window.location = 'mailto:' + email + '?subject=' + subject + '&body=' + body;
       });
     });
+
     var reload = function() {
+      $('.\\#spinner', jQ).show();
       TVRO.getSerialLog(1, 1).then(function(xml) {
         var serialLog = $('content', xml).text();
         $('.\\#serial-log', jQ).text(serialLog);
+        $('.\\#spinner', jQ).hide();
       }, function(error) {
         //  serial log isn't available yet, start the log and reload.
-        TVRO.startSerialLog({ restart: 'N' }).then(function(){setTimeout(reload,1000);})
-        });
+        TVRO.startSerialLog({ restart: 'N' }).then(function(){ setTimeout(reload,1000); });
+      });
     };
 
     return self = {
-        reload: reload
+      reload: reload
     };
   };
 
