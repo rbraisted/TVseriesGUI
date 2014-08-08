db69d2ea94ea09d23412c816938247ee34552b45
jschmidt1 <jschmidt1@localhost>
2014-08-08 19:28:57 -0400

    SMTV-446 fixing the sat table scroll so it scrolls to the installed Sat.

--- js/SatTableView.js
+++ js/SatTableView.js
@@ -15,69 +15,83 @@
     //  TVRO.TableView will remove #table-head from the DOM on creation
 
     var sortBtns = $('.\\#sort-btn', jQ)
-      .click(function() {
-        var sortBtn = $(this);
-        sortFilter = undefined;
+    .click(function() {
+      var sortBtn = $(this);
+      sortFilter = undefined;
 
-        if (sortBtn.is('.\\#name-btn')) sortFilter = 'name';
-        else if (sortBtn.is('.\\#lon-btn')) sortFilter = 'lon';
-        else if (sortBtn.is('.\\#region-btn')) sortFilter = 'region';
-        else if (sortBtn.is('.\\#fav-btn')) sortFilter = function(sat) { return !sat.favorite; };
+      if (sortBtn.is('.\\#name-btn')) {
+        sortFilter = 'name';
+      } else if (sortBtn.is('.\\#lon-btn')) {
+        sortFilter = 'lon';
+      } else if (sortBtn.is('.\\#region-btn')) {
+        sortFilter = 'region';
+      } else if (sortBtn.is('.\\#fav-btn')) {
+        sortFilter = function(sat) { return !sat.favorite; };
+      }
 
-        if (sortFilter) {
-          sortReverse = sortBtn.hasClass('$ascending');
-          sortBtns.removeClass('$ascending $descending');
-          sortBtn.toggleClass('$descending', sortReverse).toggleClass('$ascending', !sortReverse);
-        } else {
-          sortBtns.removeClass('$ascending $descending');
-        }
+      if (sortFilter) {
+        sortReverse = sortBtn.hasClass('$ascending');
+        sortBtns.removeClass('$ascending $descending');
+        sortBtn.toggleClass('$descending', sortReverse).toggleClass('$ascending', !sortReverse);
+      } else {
+        sortBtns.removeClass('$ascending $descending');
+      }
 
-        reload();
-      });
+      reload();
+    });
 
     var tableView = TVRO.TableView($('.\\#table-view', jQ))
-      .onBuild(function(row, sat) {
+    .onBuild(function(row, sat) {
 
-        $('.\\#sat-name', row).text(sat.name || 'N/A');
-        $('.\\#sat-region', row).text(sat.region || 'N/A');
-        $('.\\#sat-longitude', row).text(TVRO.formatOrbitalSlot(sat.antSatID, sat.lon) || 'N/A');
-        row.toggleClass('$favorite', sat.favorite);
+      $('.\\#sat-name', row).text(sat.name || 'N/A');
+      $('.\\#sat-region', row).text(sat.region || 'N/A');
+      $('.\\#sat-longitude', row).text(TVRO.formatOrbitalSlot(sat.antSatID, sat.lon) || 'N/A');
+      row.toggleClass('$favorite', sat.favorite);
 
-        TVRO.getSelectedSat().then(function(selectedSat) {
-          row.toggleClass('$installed', selectedSat === sat.antSatID);
-        });
+      TVRO.getSelectedSat().then(function(selectedSat) {
+        row.toggleClass('$installed', selectedSat === sat.antSatID);
+        // When we are at the row with the installed Sat, set the scroll.
+        if(selectedSat === sat.antSatID){
+          //Get the row top (relation to the page then subtract off the top of
+          //the table and table header(stationary in the table).
+          $('.\\#table-view', jQ).scrollTop(row.offset().top - ($('.\\#table-view', jQ).offset().top + $('.\\#table-view .\\table-head', jQ).height()));
+        }
+      });
 
-        $('.\\#install-btn', row).click(function() {
-          var installed = row.hasClass('$installed');
-          //  if not installed, ask for confirmation
-          var confirmed = installed ? false : confirm('Are you sure you want to install ' + sat.name + '?');
-          if (confirmed)
-            TVRO.setInstalledSat(sat);
-        });
+      $('.\\#install-btn', row).click(function() {
+        var installed = row.hasClass('$installed');
+        //  if not installed, ask for confirmation
+        var confirmed = installed ? false : confirm('Are you sure you want to install ' + sat.name + '?');
+        if (confirmed){
+          TVRO.setInstalledSat(sat);
+        }
+      });
 
-        $('.\\#fav-btn', row).click(function() {
-          row.toggleClass('$favorite');
-          TVRO.setSatelliteIdentity({
-            listID: sat.listID,
-            antSatID:sat.antSatID,
-            favorite: row.hasClass('$favorite') ? 'TRUE' : 'FALSE'
-          });
+      $('.\\#fav-btn', row).click(function() {
+        row.toggleClass('$favorite');
+        TVRO.setSatelliteIdentity({
+          listID: sat.listID,
+          antSatID:sat.antSatID,
+          favorite: row.hasClass('$favorite') ? 'TRUE' : 'FALSE'
         });
       });
+    });
 
     var reload = function() {
       TVRO.getSats().then(function(sats) {
-        if (region !== 'All') sats = _.filter(sats, { region: region });
-        if (sortFilter) sats = _.sortBy(sats, sortFilter);
-        if (sortFilter && sortReverse) sats.reverse();
+        if (region !== 'All') {
+          sats = _.filter(sats, { region: region });
+        }
 
-        var scrollTop;
-        if (region === previousRegion) scrollTop = $('.\\#table-view', jQ).scrollTop();
+        if (sortFilter) {
+          sats = _.sortBy(sats, sortFilter);
+        }
+
+        if (sortFilter && sortReverse) {
+          sats.reverse();
+        }
 
         tableView.setValues(sats).build();
-
-        if (scrollTop !== null) $('.\\#table-view', jQ).scrollTop(scrollTop);
-
       });
     };
 
