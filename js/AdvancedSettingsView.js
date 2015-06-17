e84c5554cb3cd8db4367924753f95cf7d0dd4777
Nandpal <Nandpal@localhost>
2015-06-17 18:56:06 +0530

    LNB Changes files...

--- js/AdvancedSettingsView.js
+++ js/AdvancedSettingsView.js
@@ -3,14 +3,98 @@
 
     var AdvancedSettingsView = function(jQ) {
         var self;
-
+	var lnbView        = $('.\\#lnb-view', jQ)
         var techModeBtn = TVRO.ToggleBtn(jQ.find('.\\#tech-mode-btn'))
         .onClick(function(techMode) {
             TVRO.setTechMode(techMode);
             $('.\\#tech-mode', jQ).toggle(TVRO.getTechMode());
             if (TVRO.getShellMode()) TVRO.sendShellCommand('set-tech-mode/' + techMode); //window.location = 'tvro://set-tech-mode/' + techMode;
         });
+        
+        
+        /** technician mode dropdown start **/
+        
+        var name_array = [];
+        var setSource = _.curry(function(webServiceCall, source) {
+       // console.log(webServiceCall);
+	
+		var params = {
+		                name: $('#lnb').html()
+		};
+		return webServiceCall(params);
+	    });
+        
+        var getSources = function(type, xml) {
+	   return  _.map($(type, xml), function(element) {
+	        var nameArray;
+	        nameArray = {lnb_name:$(element).text()};
+	        name_array[name_array.length] = nameArray;
+	        return nameArray.lnb_name;
+	    })
+	};
+	
+        var lnbLabel = $('.\\#lnb', lnbView);
+        var lnbDropdownView = TVRO.DropdownView($('.\\#lnb-dropdown-view'));
+        var showLnbDropdownView = function(val) {
+        
+        	TVRO.getLnbList().then(function(xml) {
+        	$('.lnb').html("");
+        	
+        	var selectedPart;
+			_.map($('installed_lnb', xml), function(installed) {
+				_.map($('part', installed), function(part) {
+					//alert($(part).text());
+					selectedPart = $(part).text();
+				})
+			})
+		
+			_.map($('lnb_list', xml), function(element) {
 
+				var listSources = getSources('name', element);
+		       		for(var i=0; i<listSources.length; i++)
+		       		{
+		       			//alert(listSources[i].split(" ")[0]);
+		       			if(selectedPart == listSources[i].split(" ")[0])
+		       			{
+		       				$('.\\#lnb').html(listSources[i]);
+		       				$("<div class='table-row lnb-row #table-row $selected'><span class='table-col dropdown-icon'></span><span class='table-col #dropdown-value'>"+listSources[i]+"</span></div>").appendTo(".lnb");
+		       			}
+		       			else
+		       			{
+		       				$("<div class='table-row lnb-row #table-row'><span class='table-col dropdown-icon'></span><span class='table-col #dropdown-value'>"+listSources[i]+"</span></div>").appendTo(".lnb");
+		       			}
+		       		}
+			})
+
+                });
+                if(val == "hide")
+                {
+                	lnbDropdownView.hide();
+                }
+                else
+                {
+                	lnbDropdownView.show();
+                }
+
+            setTimeout(function() {
+            	 $('.lnb-row').click(function(){
+			 $('.lnb-row').removeClass('$selected');
+			 $(this).addClass('$selected');
+			$('.\\#lnb').html($(this).children().next().html());
+			TVRO.DropdownView($('.\\#lnb-dropdown-view')).hide();
+			setSource(TVRO.setLnb)
+		});
+            
+            }, 500);
+        };
+        
+        showLnbDropdownView("hide");
+        var lnbDropdownBtn = $('.\\#lnb-btn', lnbView).click(showLnbDropdownView);
+        
+        
+	/** technician mode dropdown end **/
+	
+	
         var saveBtn = $('.\\#save-btn', jQ).click(function() {
             sessionStorage['kvhupdate'] = $('.\\#update-url', jQ).val();
         });
